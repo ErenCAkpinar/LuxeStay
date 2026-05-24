5d2234a8d2b99cab6e5a728b2f1edb62c1f89bed
EgeSoykara <140652188+EgeSoykara@users.noreply.github.com>
2026-05-24 14:56:49 +0300

    Delete LuxeStay4 directory

--- LuxeStay4/luxestay/wsgi.py
+++
@@ -1,9 +0,0 @@
-"""WSGI config for the LuxeStay project."""
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "luxestay.settings")
-
-application = get_wsgi_application()
